2eb210225fb79c3ce8bf20667ca36eaea99fceee
josaiLopz <92553935+josaiLopz@users.noreply.github.com>
2021-11-07 22:38:21 -0600

    prueba cache
    modificaciones de cache por fallos en la app.

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v4';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const STATIC_CACHE = 'static-v5';
+const DYNAMIC_CACHE = 'dynamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL =[
